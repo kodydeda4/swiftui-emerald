0a4afc19ce131dc9757d2b5c1307344369b485fa
Kody Deda <kodydeda4@gmail.com>
2021-02-07 12:47:49 -0500

    Initial Commit

---
+++ YabaiUI/YabaiUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YabaiUIApp.swift
+//  YabaiUI
+//
+//  Created by Kody Deda on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct YabaiUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
