c292cf401b19cd745f68af8daa1361b77f4de51b
Kody Deda <kodydeda4@gmail.com>
2021-02-10 07:59:04 -0500

    created actions for Yabai Space Settings

--- YabaiUI/State/Yabai.swift
+++ YabaiUI/State/Yabai.swift
@@ -7,22 +7,43 @@
 //------------------------------------------------------------------------
 // Yabai
 // - A tiling window manager for macOS based on binary space partitioning
-//   https://github.com/koekeishiya/yabai/
+//   https://github.com/koekeishiya/yabai/blob/master/doc/yabai.asciidoc
 //------------------------------------------------------------------------
 
 //MARK:- Yabai
 
+struct YabaiSpaceSettings: Equatable, Codable {
+    enum Layout: String, Codable, CaseIterable, Identifiable {
+        case bsp
+        case stack
+        case float
+        
+        var id: Layout { self }
+    }
+    
+    var layout: Layout = .float
+    var topPadding: Int = 0
+    var bottomPadding: Int = 0
+    var leftPadding: Int = 0
+    var rightPadding: Int = 0
+    var windowGap: Int = 0
+}
+
 import SwiftUI
 import ComposableArchitecture
 
-
 struct Yabai {
     struct State: Equatable, Codable {
-        var yabaiString: String = ""
+        var yabaiSpaceSettings = YabaiSpaceSettings()
     }
     
     enum Action: Equatable {
-        case updateYabaiString(String)
+        case updateLayout(YabaiSpaceSettings.Layout)
+        case updateTopPadding(Int)
+        case updateBottomPadding(Int)
+        case updateLeftPadding(Int)
+        case updateRightPadding(Int)
+        case updateWindowGap(Int)
     }
     
     struct Environment {
@@ -35,10 +56,29 @@ extension Yabai {
         Reducer { state, action, environment in
             switch action {
             
-            case let .updateYabaiString(string):
-                state.yabaiString = string
+            case let .updateLayout(layout):
+                state.yabaiSpaceSettings.layout = layout
                 return .none
                 
+            case let .updateTopPadding(int):
+                state.yabaiSpaceSettings.topPadding = int
+                return.none
+                
+            case let .updateBottomPadding(int):
+                state.yabaiSpaceSettings.bottomPadding = int
+                return.none
+                
+            case let .updateLeftPadding(int):
+                state.yabaiSpaceSettings.leftPadding = int
+                return.none
+                
+            case let .updateRightPadding(int):
+                state.yabaiSpaceSettings.rightPadding = int
+                return.none
+                
+            case let .updateWindowGap(int):
+                state.yabaiSpaceSettings.windowGap = int
+                return.none
             }
         }
     )
@@ -59,15 +99,64 @@ struct YabaiView: View {
     
     var body: some View {
         WithViewStore(store) { viewStore in
-            List {
-                Text("YabaiString")
-                    .foregroundColor(.gray)
+            Form {
+                Picker("Layout", selection:
+                        viewStore.binding(
+                            get: \.yabaiSpaceSettings.layout,
+                            send: Yabai.Action.updateLayout)
+                ) {
+                    ForEach(YabaiSpaceSettings.Layout.allCases) {
+                        Text($0.rawValue)
+                    }
+                    
+                }
+                HStack {
+                    Text("Top Padding")
+                    TextField(
+                        "",
+                        value: viewStore.binding(
+                            get: \.yabaiSpaceSettings.topPadding,
+                            send: Yabai.Action.updateTopPadding),
+                        formatter: NumberFormatter()
+                    )
+                }
                 
-                TextField("Untitled", text: viewStore.binding(
-                    get: \.yabaiString,
-                    send: Yabai.Action.updateYabaiString
-                ))
+                HStack {
+                    Text("Bottom Padding")
+                    TextField(
+                        "",
+                        value: viewStore.binding(
+                            get: \.yabaiSpaceSettings.bottomPadding,
+                            send: Yabai.Action.updateBottomPadding
+                        ),
+                        formatter: NumberFormatter()
+                    )
+                }
+                
+                HStack {
+                    Text("Left Padding")
+                    TextField(
+                        "",
+                        value: viewStore.binding(
+                            get: \.yabaiSpaceSettings.leftPadding,
+                            send: Yabai.Action.updateLeftPadding),
+                        formatter: NumberFormatter()
+                    )
+                }
+                
+                HStack {
+                    Text("Right Padding")
+                    TextField(
+                        "",
+                        value: viewStore.binding(
+                            get: \.yabaiSpaceSettings.rightPadding,
+                            send: Yabai.Action.updateRightPadding
+                        ),
+                        formatter: NumberFormatter()
+                    )
+                }
             }
+            .padding()
         }
     }
 }
