01cd7e12d9820f599d08babedb3a3d85f22118b7
Kody Deda <kodydeda4@gmail.com>
2021-02-11 21:15:56 -0500

    icons - app has a basic icon, onboaring has that icon

--- YabaiUI/State/Onboarding.swift
+++ YabaiUI/State/Onboarding.swift
@@ -46,6 +46,61 @@ extension Onboarding {
     )
 }
 
+
+struct VStackOfTextViews: View {
+    
+    var body: some View {
+        VStack {
+            Group {
+                Text("largeTitle")
+                    .font(.largeTitle)
+                
+                Text("title")
+                    .font(.title)
+                
+                Text("title2")
+                    .font(.title2)
+                
+                Text("title3")
+                    .font(.title3)
+            }
+            
+            Group {
+                Text("subheadline")
+                    .font(.subheadline)
+                
+                Text("headline")
+                    .font(.headline)
+            }
+            
+            Group {
+                Text("body")
+                    .font(.body)
+                
+                Text("caption")
+                    .font(.caption)
+                
+                Text("caption2")
+                    .font(.caption2)
+                
+                Text("footnote")
+                    .font(.footnote)
+                
+                Text("callout")
+                    .font(.callout)
+                
+            }
+        }
+    }
+}
+
+struct VStackOfTextViews_Previews: PreviewProvider {
+    static var previews: some View {
+        VStackOfTextViews()
+    }
+}
+
+
 // MARK:- OnboardingView
 
 struct OnboardingView: View {
@@ -54,11 +109,18 @@ struct OnboardingView: View {
     var body: some View {
         WithViewStore(store) { viewStore in
             VStack {
+                Image("iconLogo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                
+                Text("Welcome to YabaiUI")
+                    .font(.largeTitle)
+                
+                
                 Button("Contine") {
                     viewStore.send(.toggleDismissed)
                 }
             }
-            //.frame(width: 300, height: 200)
             .padding()
         }
     }
