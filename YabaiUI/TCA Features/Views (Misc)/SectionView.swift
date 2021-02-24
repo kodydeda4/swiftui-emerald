7a6aed70cdaa0c5d968ce09e3ffdcfc561b0c4a4
Kody Deda <kodydeda4@gmail.com>
2021-02-24 15:32:39 -0500

    grouped misc views

---
+++ YabaiUI/TCA Features/Views (Misc)/SectionView.swift
@@ -0,0 +1,45 @@
+//
+//  SectionView.swift
+//  YabaiUI
+//
+//  Created by Kody Deda on 2/24/21.
+//
+
+import SwiftUI
+
+struct SectionView<Content: View>: View {
+    let title: String
+    let content: Content
+    
+    init(_ title: String, @ViewBuilder content: () -> Content) {
+        self.title = title
+        self.content = content()
+    }
+    
+    var body: some View {
+        Section {
+            VStack(alignment: .leading) {
+                Text(title).fontWeight(.bold)
+                Divider()
+                content
+            }
+            .padding()
+        }
+    }
+}
+
+struct SectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SectionView("Title") {
+            Picker("View A", selection: .constant("Breakfast")) {
+                ForEach(["Breakfast", "Lunch", "Dinner"], id: \.self) {
+                    Text($0)
+                }
+            }
+            HStack {
+                Text("View B")
+                TextField("", text: .constant("25"))
+            }
+        }
+    }
+}
---
+++ YabaiUI/TCA Features/Views (Misc)/SpecialTextView.swift
@@ -0,0 +1,74 @@
+//
+//  SpecialTextView.swift
+//  YabaiUI
+//
+//  Created by Kody Deda on 2/24/21.
+//
+
+import SwiftUI
+
+/*
+ TODO:
+ 
+ Merge into one.
+ 
+ */
+
+// MARK:- SwiftUI Previews
+struct SpecialTextField: View {
+    let title: String
+    @Binding var value: Int
+    let formatter: Formatter = NumberFormatter()
+    
+    
+    var body: some View {
+        HStack {
+            Text(title)
+            TextField("", value: $value, formatter: formatter)
+        }
+    }
+}
+
+struct SpecialTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        SpecialTextField(title: "Value", value: .constant(64))
+    }
+}
+
+// MARK:- SwiftUI Previews
+struct SpecialTextFieldStrings: View {
+    let title: String
+    @Binding var value: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+            TextField("", text: $value)
+        }
+    }
+}
+
+struct SpecialTextFieldStrings_Previews: PreviewProvider {
+    static var previews: some View {
+        SpecialTextFieldStrings(title: "Value", value: .constant("Hello"))
+    }
+}
+// MARK:- SwiftUI Previews
+struct SpecialTextFieldFloats: View {
+    let title: String
+    @Binding var value: Float
+    let formatter: Formatter = NumberFormatter()
+    
+    var body: some View {
+        HStack {
+            Text(title)
+            TextField("", value: $value, formatter: formatter)
+        }
+    }
+}
+
+struct SpecialTextFieldFloats_Previews: PreviewProvider {
+    static var previews: some View {
+        SpecialTextFieldFloats(title: "Value", value: .constant(64.0))
+    }
+}
