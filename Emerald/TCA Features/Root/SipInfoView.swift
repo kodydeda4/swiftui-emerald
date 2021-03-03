878959a65728d5d005a49df5606410905b51325a
Kody Deda <kodydeda4@gmail.com>
2021-03-03 10:16:47 -0500

    more info about SIP

--- Emerald/TCA Features/Root/SipInfoView.swift
+++ Emerald/TCA Features/Root/SipInfoView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import OmenTextField
 
 let sectionA =
 """
@@ -16,7 +17,7 @@ files and directories even to root users or those with root privileges (sudo).
 let a2 =
 """
 System Integrity Protection must be partially disabled to establish a connection with the macOS window server.
-The following features require SIP to be partially disabled to work:
+The following features require partially disabling System Integrity Protection:
 
 • Window Shadows
 • Window Transparency
@@ -28,10 +29,18 @@ The following features require SIP to be partially disabled to work:
 
 let description2 =
 """
-1. Restart your Mac while holding Command-R to boot into recovery mode.
+1. Boot into recovery mode by restarting your Mac while holding Command-R.
 2. Open a Terminal from the menubar (Utilities > Terminal).
-3. Run the command specific to your version of macOS:
+3. Run the specific command for your version of macOS.
+4. Reboot to exit recovery mode and apply changes.
 """
+
+let installAndLoadCommands =
+"""
+sudo yabai --load-sa
+"""
+
+
 
 //What is System Integrity Protection and why does it need to be disabled?
 //https://github.com/koekeishiya/yabai/wiki/Disabling-System-Integrity-Protection
@@ -46,7 +55,7 @@ struct SipInfoView: View {
         List {
             Group {
                 Text("What is System Integrity Protection?")
-                    .bold()
+                    .font(.title3)
                 Text(sectionA)
                     .foregroundColor(.gray)
                 
@@ -55,19 +64,18 @@ struct SipInfoView: View {
             Divider()
             Group {
                 Text("Why disable it?")
-                    .bold()
+                    .font(.title3)
                 Text(a2)
                     .foregroundColor(.gray)
             }
             Divider()
             Group {
                 Text("Disabling System Integrity Protection")
-                    .bold()
+                    .font(.title3)
+                
                 Text(description2)
                     .foregroundColor(.gray)
-            }
             
-            Group {
                 Text("High Sierra")
                 TextField("", text: .constant("csrutil disable"))
                     .foregroundColor(.gray)
@@ -82,6 +90,70 @@ struct SipInfoView: View {
                 TextField("", text: .constant("csrutil disable --with kext --with dtrace --with nvram --with basesystem"))
                     .foregroundColor(.gray)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            Divider()
+            Group {
+                Text("Final Steps")
+                    .font(.title3)
+                
+                Text("After disabling System Integrity Protection, you need to install and load the scripting additions.")
+                    .foregroundColor(.gray)
+                
+                Text("Open Terminal and run the following command:")
+                    .foregroundColor(.gray)
+            
+                TextField("", text: .constant("sudo yabai --install-sa"))
+                    .foregroundColor(.gray)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            }
+            Divider()
+            Group {
+                Text("Extra Steps for macOS 11.01 BigSur and above")
+                    .font(.title3)
+                
+                HStack {
+                    Text("If you're running macOS 11.01+ you'll have to edit a file with root priviledges to load the scripting additions automatically.")
+                    .foregroundColor(.gray)
+                    
+                    Link("Learn more", destination: URL(string: "https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)#macos-big-sur---automatically-load-scripting-addition-on-startup")!)
+                }
+                
+                Text("Open Terminal and run the following commands:")
+                
+                Text("1. Find your username")
+                    .foregroundColor(.gray)
+                
+                TextField("", text: .constant("whoami"))
+                    .foregroundColor(.gray)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                
+                Text("2. Open the file with visudo")
+                    .foregroundColor(.gray)
+                
+                Group {
+                
+                TextField("", text: .constant("sudo visudo -f /private/etc/sudoers.d/yabai"))
+                    .foregroundColor(.gray)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Text("3. Press enter to switch to Insertion Mode")
+                    .foregroundColor(.gray)
+                
+                Text("4. Write the following, replacing 'user' with your username")
+                    .foregroundColor(.gray)
+
+                TextField("", text: .constant("user ALL = (root) NOPASSWD: /usr/local/bin/yabai --load-sa"))
+                    .foregroundColor(.gray)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                Text("5. Press esc to exit Insertion Mode")
+                    .foregroundColor(.gray)
+                    
+                Text("6. Type :wq to save changes and exit visudo")
+                    .foregroundColor(.gray)
+                    
+                }
             }
         }
         .navigationTitle("System Integrity Protection")
