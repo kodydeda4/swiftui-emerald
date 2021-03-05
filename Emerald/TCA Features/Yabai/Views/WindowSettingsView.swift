28b4d76e191ded0fdc659beff515abd57d50fb07
Kody Deda <kodydeda4@gmail.com>
2021-03-05 11:31:45 -0500

    building out UI

--- Emerald/TCA Features/Yabai/Views/WindowSettingsView.swift
+++ Emerald/TCA Features/Yabai/Views/WindowSettingsView.swift
@@ -24,63 +24,82 @@ struct WindowSettingsView: View {
                     }.labelsHidden()
                 }
                 Divider()
-                Section(
-                    header:
-                        HStack {
+                Section(header: Text("Opacity Effects").bold()) {
+                    HStack {
+                        VStack {
                             Toggle("Opacity Effects", isOn: viewStore.binding(keyPath: \.windowOpacity, send: keyPath)).labelsHidden()
-                            Text("Opacity Effects")
+                            Text("")
                         }
-                ) {
-                    VStack {
-                        SpecialTextFieldFloats(title: "Animation Duration", value: viewStore.binding(keyPath: \.windowOpacityDuration, send: keyPath))
-                        
-                        VStack(alignment: .leading) {
-                            Text("Focused Window Opacity \(viewStore.activeWindowOpacity)")
-                            Slider(value: viewStore.binding(keyPath: \.activeWindowOpacity, send: keyPath), in: 0.0...1.0)//, step: 0.1)
-                        }
-                        VStack(alignment: .leading) {
-                            Text("Normal Window Opacity \(viewStore.activeWindowOpacity)")
-                            Slider(value: viewStore.binding(keyPath: \.normalWindowOpacity, send: keyPath), in: 0.0...1.0)//, step: 0.1)
-                        }
-                    }.disabled(!viewStore.windowOpacity)
-                    
+                        Group {
+                            VStack(alignment: .leading) {
+                                Slider(value: viewStore.binding(keyPath: \.windowOpacityDuration, send: keyPath), in: 0.0...1.0)
+                                Text("Animation Duration \(Int(viewStore.windowOpacityDuration * 100))%")
+                            }
+                            
+                            VStack(alignment: .leading) {
+                                Slider(value: viewStore.binding(keyPath: \.activeWindowOpacity, send: keyPath), in: 0.0...1.0)
+                                Text("Focused Window \(Int(viewStore.activeWindowOpacity * 100))%")
+                            }
+                            VStack(alignment: .leading) {
+                                Slider(value: viewStore.binding(keyPath: \.normalWindowOpacity, send: keyPath), in: 0.0...1.0)
+                                Text("Normal Windows \(Int(viewStore.normalWindowOpacity * 100))%")
+                            }
+                        }.disabled(!viewStore.windowOpacity)
+                    }
                 }
                 Divider()
-                Section(
-                    header:
-                        HStack {
-                            Toggle("Window Borders", isOn: viewStore.binding(keyPath: \.windowBorder, send: keyPath)).labelsHidden()
-                            Text("Window Borders")
-                        }
-                ) {
+                Section(header: Text("Borders").bold()) {
                     HStack {
-                        Stepper("Width \(viewStore.windowBorderWidth)", value: viewStore.binding(keyPath: \.windowBorderWidth, send: keyPath), in: 0...10)
-                            .padding(6)
-                            .background(Color.black.opacity(0.25))
-                            .clipShape(RoundedRectangle(cornerRadius: 8))
-                        
-                        ColorPicker("Focused", selection: viewStore.binding(get: \.activeWindowBorderColor.color, send: Yabai.Action.updateActiveWindowBorderColor))
-                        ColorPicker("Normal", selection: viewStore.binding(get: \.normalWindowBorderColor.color, send: Yabai.Action.updateNormalWindowBorderColor))
-                        ColorPicker("Insert", selection: viewStore.binding(get: \.insertWindowBorderColor.color, send: Yabai.Action.updateInsertWindowBorderColor))
-                    }
-                    .disabled(!viewStore.windowBorder)
-                }
-                VStack(alignment: .leading) {
-                    Text("Placement")
-                    Picker("Placement", selection: viewStore.binding(keyPath: \.windowPlacement, send: keyPath)) {
-                        ForEach(Yabai.State.WindowPlacement.allCases) {
-                            Text($0.rawValue)
+                        VStack {
+                            Toggle("Borders", isOn: viewStore.binding(keyPath: \.windowBorder, send: keyPath)).labelsHidden()
+                            Text("")
                         }
-                    }.labelsHidden()
+                        
+                        Group {
+                            VStack {
+                                HStack {
+                                    Text("\(viewStore.windowBorderWidth)")
+                                    Spacer()
+                                    Stepper("", value: viewStore.binding(keyPath: \.windowBorderWidth, send: keyPath), in: 0...10)
+                                }
+                                .frame(width: 60)
+                                .background(Color.black.opacity(0.25))
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+
+                                Text("Width")
+                            }
+                            VStack {
+                                ColorPicker("Focused", selection: viewStore.binding(get: \.activeWindowBorderColor.color, send: Yabai.Action.updateActiveWindowBorderColor)).labelsHidden()
+                                Text("Focused")
+                            }
+                            VStack {
+                                ColorPicker("Normal", selection: viewStore.binding(get: \.normalWindowBorderColor.color, send: Yabai.Action.updateNormalWindowBorderColor)).labelsHidden()
+                                Text("Normal")
+                            }
+                            VStack {
+                                ColorPicker("Insert", selection: viewStore.binding(get: \.insertWindowBorderColor.color, send: Yabai.Action.updateInsertWindowBorderColor)).labelsHidden()
+                                Text("Insert")
+                            }
+                        }
+                        .disabled(!viewStore.windowBorder)
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("Placement")
+                        Picker("Placement", selection: viewStore.binding(keyPath: \.windowPlacement, send: keyPath)) {
+                            ForEach(Yabai.State.WindowPlacement.allCases) {
+                                Text($0.rawValue)
+                            }
+                        }.labelsHidden()
+                    }
+                    Toggle("Floating Windows Stay-On-Top", isOn: viewStore.binding(keyPath: \.windowTopmost, send: keyPath))
+                    Toggle("Auto Balance", isOn: viewStore.binding(keyPath: \.autoBalance, send: keyPath))
+                    SpecialTextFieldFloats(title: "Split Ratio", value: viewStore.binding(keyPath: \.splitRatio, send: keyPath)).disabled(viewStore.autoBalance)
                 }
-                Toggle("Floating Windows Stay-On-Top", isOn: viewStore.binding(keyPath: \.windowTopmost, send: keyPath))
-                Toggle("Auto Balance", isOn: viewStore.binding(keyPath: \.autoBalance, send: keyPath))
-                SpecialTextFieldFloats(title: "Split Ratio", value: viewStore.binding(keyPath: \.splitRatio, send: keyPath)).disabled(viewStore.autoBalance)
             }
         }
     }
 }
-
 // MARK:- SwiftUI_Previews
 struct WindowSettingsView_Previews: PreviewProvider {
     static var previews: some View {
