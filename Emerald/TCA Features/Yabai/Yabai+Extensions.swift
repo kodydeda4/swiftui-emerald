fd1478abd6bd913f8cb20407d6d7f8c02c7e6c41
Kody Deda <kodydeda4@gmail.com>
2021-03-08 09:45:08 -0500

    yabairc file betterreflects view

--- Emerald/TCA Features/Yabai/Yabai+Extensions.swift
+++ Emerald/TCA Features/Yabai/Yabai+Extensions.swift
@@ -25,59 +25,17 @@ extension Yabai.State {
 #
 
 #=========================================================================================
-# TEMPORARILY HARDCODED
-#=========================================================================================
-yabai -m rule --add label=\"System Preferences\" app=\"^System Preferences$\" manage=off"
-
-#=========================================================================================
-# General
+# Debug
 #=========================================================================================
 yabai -m config debug_output \(debugOutput == true ? "on" : "off")
-yabai -m config external_bar \(externalBar):\(externalBarPaddingTop):\(externalBarPaddingBottom)
-yabai -m config mouse_follows_focus \(mouseFollowsFocus == true ? "on" : "off")
-yabai -m config focus_follows_mouse \(focusFollowsMouse)
 
 #=========================================================================================
-# Window Misc
+# Space
 #=========================================================================================
-yabai -m config window_placement \(windowPlacement)
-yabai -m config window_shadow \(windowShadow)
-
-#=========================================================================================
-# Window Opacity
-#=========================================================================================
-yabai -m config window_opacity \(windowOpacity == true ? "on" : "off")
-yabai -m config window_opacity_duration \(Float(windowOpacityDuration)/100)
-yabai -m config active_window_opacity \(Float(activeWindowOpacity)/100)
-yabai -m config normal_window_opacity \(Float(normalWindowOpacity)/100)
-
-#=========================================================================================
-# Window Borders
-#=========================================================================================
-yabai -m config window_border \(windowBorder == true ? "on" : "off")
-yabai -m config window_border_width \(windowBorderWidth)
-yabai -m config active_window_border_color \"\(activeWindowBorderColor.asHexString)\"
-yabai -m config normal_window_border_color \"\(normalWindowBorderColor.asHexString)\"
-yabai -m config insert_window_border_color \"\(insertWindowBorderColor.asHexString)\"
-
-#=========================================================================================
-# Misc
-#=========================================================================================
-yabai -m config split_ratio \(Float(splitRatio/100))
-yabai -m config auto_balance \(autoBalance == true ? "on" : "off")
-
-#=========================================================================================
-# Mouse Actions",
-#=========================================================================================
-yabai -m config mouse_modifier \(mouseModifier)
-yabai -m config mouse_action1 \(mouseAction1)
-yabai -m config mouse_action2 \(mouseAction2)
-yabai -m config mouse_drop_action \(mouseDropAction)
-
-#=========================================================================================
-# Space Settings
-#=========================================================================================
+# Layout
 yabai -m config layout \(layout)
+
+# Padding
 yabai -m config top_padding \(paddingTop)
 yabai -m config bottom_padding \(paddingBottom)
 yabai -m config left_padding \(paddingLeft)
@@ -85,15 +43,72 @@ yabai -m config right_padding \(paddingRight)
 yabai -m config window_gap \(windowGap)
 
 #=========================================================================================
-# Load Scripting Addition
+# Window
 #=========================================================================================
-sudo yabai --load-sa
-yabai -m signal --add event=dock_did_restart action=\"sudo yabai --load-sa\"
+# Shadows Effects
+yabai -m config window_shadow \(windowShadow)
+
+# Opacity Effects
+yabai -m config window_opacity \(windowOpacity == true ? "on" : "off")
+yabai -m config window_opacity_duration \(Float(windowOpacityDuration)/100)
+yabai -m config active_window_opacity \(Float(activeWindowOpacity)/100)
+yabai -m config normal_window_opacity \(Float(normalWindowOpacity)/100)
+
+# Borders
+yabai -m config window_border \(windowBorder == true ? "on" : "off")
+yabai -m config window_border_width \(windowBorderWidth)
+yabai -m config active_window_border_color \"\(activeWindowBorderColor.asHexString)\"
+yabai -m config normal_window_border_color \"\(normalWindowBorderColor.asHexString)\"
+yabai -m config insert_window_border_color \"\(insertWindowBorderColor.asHexString)\"
+
+# New Window Placement
+yabai -m config window_placement \(windowPlacement)
+
+# Auto Balance
+yabai -m config auto_balance \(autoBalance == true ? "on" : "off")
+
+# Split Ratio
+yabai -m config split_ratio \(Float(splitRatio/100))
+
+# Floating Windows Stay-On-Top
+yabai -m config window_topmost \(windowTopmost == true ? "on" : "off") #SA
 
 #=========================================================================================
-# Scripting Addition Options
+# Mouse
 #=========================================================================================
-yabai -m config window_topmost \(windowTopmost == true ? "on" : "off")
+# Mouse Follows Focus
+yabai -m config mouse_follows_focus \(mouseFollowsFocus == true ? "on" : "off")
+
+# Focus Follows Mouse
+yabai -m config focus_follows_mouse \(focusFollowsMouse)
+
+# Modifier Key
+yabai -m config mouse_modifier \(mouseModifier)
+
+# Left Click + Modifier
+yabai -m config mouse_action1 \(mouseAction1)
+
+# Right Click + Modifier
+yabai -m config mouse_action2 \(mouseAction2)
+
+# Drop Action
+yabai -m config mouse_drop_action \(mouseDropAction)
+
+#=========================================================================================
+# External Bar
+#=========================================================================================
+# External Bar top:bottom
+yabai -m config external_bar \(externalBar):\(externalBarPaddingTop):\(externalBarPaddingBottom)
+
+#=========================================================================================
+# Misc
+#=========================================================================================
+# Set-System Preferences Floating
+yabai -m rule --add label=\"System Preferences\" app=\"^System Preferences$\" manage=off"
+
+# Load Scripting Addition
+sudo yabai --load-sa
+yabai -m signal --add event=dock_did_restart action=\"sudo yabai --load-sa\"
 """
     }
 }
