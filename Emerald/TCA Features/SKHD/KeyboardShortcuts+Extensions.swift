0fa139f9c377d98d6eaf6c11ca5f7bfd651482c2
Kody Deda <kodydeda4@gmail.com>
2021-04-22 11:56:47 -0400

    removed whitespace from kbshortcuts

--- Emerald/TCA Features/SKHD/KeyboardShortcuts+Extensions.swift
+++ Emerald/TCA Features/SKHD/KeyboardShortcuts+Extensions.swift
@@ -9,30 +9,30 @@ import SwiftUI
 import KeyboardShortcuts
 
 extension KeyboardShortcuts.Name {
-    static let float          = Self("Normal")
-    static let bsp            = Self("Tiling")
-    static let stack          = Self("Stacking")
-    static let focus          = Self("Focus")
-    static let resize         = Self("Resize")
-    static let move           = Self("Move")
-    static let gaps           = Self("Gaps")
-    static let padding        = Self("Padding")
-    static let split          = Self("Split")
-    static let balance        = Self("Balance")
+    static let float   = Self("Normal")
+    static let bsp     = Self("Tiling")
+    static let stack   = Self("Stacking")
+    static let focus   = Self("Focus")
+    static let resize  = Self("Resize")
+    static let move    = Self("Move")
+    static let gaps    = Self("Gaps")
+    static let padding = Self("Padding")
+    static let split   = Self("Split")
+    static let balance = Self("Balance")
 }
 
 extension KeyboardShortcuts {
     static func resetEmeraldDefaults() {
-        KeyboardShortcuts.set(.float,          [.control, .option,          ], .one)
-        KeyboardShortcuts.set(.bsp,            [.control, .option,          ], .two)
-        KeyboardShortcuts.set(.stack,          [.control, .option,          ], .three)
-        KeyboardShortcuts.set(.focus,          [.control,                   ], .help)
-        KeyboardShortcuts.set(.resize,         [.control, .option,          ], .help)
-        KeyboardShortcuts.set(.move,           [.control, .option, .command ], .help)
-        KeyboardShortcuts.set(.gaps,           [.control, .option,          ], .g)
-        KeyboardShortcuts.set(.padding,        [.control, .option,          ], .h)
-        KeyboardShortcuts.set(.split,          [.control, .option,          ], .x)
-        KeyboardShortcuts.set(.balance,        [.control, .option,          ], .b)
+        KeyboardShortcuts.set(.float,   [.control, .option,          ], .one)
+        KeyboardShortcuts.set(.bsp,     [.control, .option,          ], .two)
+        KeyboardShortcuts.set(.stack,   [.control, .option,          ], .three)
+        KeyboardShortcuts.set(.focus,   [.control,                   ], .help)
+        KeyboardShortcuts.set(.resize,  [.control, .option,          ], .help)
+        KeyboardShortcuts.set(.move,    [.control, .option, .command ], .help)
+        KeyboardShortcuts.set(.gaps,    [.control, .option,          ], .g)
+        KeyboardShortcuts.set(.padding, [.control, .option,          ], .h)
+        KeyboardShortcuts.set(.split,   [.control, .option,          ], .x)
+        KeyboardShortcuts.set(.balance, [.control, .option,          ], .b)
     }
 }
 
