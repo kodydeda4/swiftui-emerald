e8b7cdbf8104040ed2a280d5d89a920db01bffd6
Kody Deda <kodydeda4@gmail.com>
2021-04-26 10:39:57 -0400

    added more stuffs.

--- Emerald/TCA Features/Yabai/Views/PlacementSettingsView.swift
+++ Emerald/TCA Features/Yabai/Views/PlacementSettingsView.swift
@@ -15,74 +15,54 @@ struct PlacementSettingsView: View {
     var body: some View {
         WithViewStore(store) { viewStore in
             ScrollView {
-                //Toggles
                 VStack(alignment: .leading) {
-                    Text("Shortcuts")
-                        .bold().font(.title3)
-                    
-                    HStack {
-                        Text("Restart Yabai")
-                        //KeyboardShortcuts.Recorder(for: .restartYabai)
-                    }
-                    VStack(alignment: .leading) {
-                        Divider()
-                        Text("Toggles")
-                            .bold().font(.title3)
-                        
-                        HStack {
-                            Text("Toggle Splt")
-                            KeyboardShortcuts.Recorder(for: .split)
+                    SectionView("Placement") {
+                        // Placement
+                        VStack(alignment: .leading) {
+                            Text("Placement")
+                                .bold().font(.title3)
+                            
+                            Picker("", selection: viewStore.binding(keyPath: \.windowPlacement, send: Yabai.Action.keyPath)) {
+                                ForEach(Yabai.State.WindowPlacement.allCases) {
+                                    Text($0.labelDescription.lowercased())
+                                }
+                            }
+                            .labelsHidden()
+                            .pickerStyle(SegmentedPickerStyle())
+                            .frame(width: 200)
+                            
+                            Text(viewStore.windowPlacement.caseDescription)
+                                .foregroundColor(Color(.gray))
                         }
-                        HStack {
-                            Text("Toggle Balance")
-                            KeyboardShortcuts.Recorder(for: .balance)
-                        }
-                    }
-                }
-                // Placement
-                VStack(alignment: .leading) {
-                    Text("Placement")
-                        .bold().font(.title3)
-                    
-                    Picker("", selection: viewStore.binding(keyPath: \.windowPlacement, send: Yabai.Action.keyPath)) {
-                        ForEach(Yabai.State.WindowPlacement.allCases) {
-                            Text($0.labelDescription.lowercased())
-                        }
-                    }
-                    .labelsHidden()
-                    .pickerStyle(SegmentedPickerStyle())
-                    .frame(width: 200)
-                    
-                    Text(viewStore.windowPlacement.caseDescription)
-                        .foregroundColor(Color(.gray))
-                }
-                // Split Ratio
-                VStack(alignment: .leading) {
-                    Divider()
-                    
-                    Text("Split Ratio")
-                        .bold().font(.title3)
-                    
-                    Picker("", selection: viewStore.binding(keyPath: \.windowBalance, send: Yabai.Action.keyPath)) {
-                        ForEach(Yabai.State.WindowBalance.allCases) {
-                            Text($0.labelDescription.lowercased())
+                        // Split Ratio
+                        VStack(alignment: .leading) {
+                            Divider()
+                            
+                            Text("Split Ratio")
+                                .bold().font(.title3)
+                            
+                            Picker("", selection: viewStore.binding(keyPath: \.windowBalance, send: Yabai.Action.keyPath)) {
+                                ForEach(Yabai.State.WindowBalance.allCases) {
+                                    Text($0.labelDescription.lowercased())
+                                }
+                            }
+                            .labelsHidden()
+                            .pickerStyle(SegmentedPickerStyle())
+                            .frame(width: 200)
+                            
+                            Slider(value: viewStore.binding(keyPath: \.splitRatio, send: Yabai.Action.keyPath))
+                                .disabled(viewStore.windowBalance != .custom)
+                                .opacity(viewStore.windowBalance != .custom ? 0.5 : 1.0)
+                            
+                            Text(viewStore.windowBalance.caseDescription)
+                                .foregroundColor(Color(.gray))
                         }
                     }
-                    .labelsHidden()
-                    .pickerStyle(SegmentedPickerStyle())
-                    .frame(width: 200)
-                    
-                    Slider(value: viewStore.binding(keyPath: \.splitRatio, send: Yabai.Action.keyPath))
-                        .disabled(viewStore.windowBalance != .custom)
-                        .opacity(viewStore.windowBalance != .custom ? 0.5 : 1.0)
-
-                    Text(viewStore.windowBalance.caseDescription)
-                        .foregroundColor(Color(.gray))
                 }
             }
-            .frame(alignment: .leading)
+            .frame(maxWidth: 1200)
             .padding()
-            .navigationTitle("Space")
+            .navigationTitle("Placement")
         }
     }
 }
